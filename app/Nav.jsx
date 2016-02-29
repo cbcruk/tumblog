0b8512e8a31730025cfc19e476ac0cf76790fb86
Eunsoo Lee <cbcruk@gmail.com>
2016-02-29 09:57:16 +0900

    h2 텍스트 수정

--- app/Nav.jsx
+++ app/Nav.jsx
@@ -12,11 +12,7 @@ class Nav extends Component {
         role="navigation"
         aria-labelledby="paginglabel"
       >
-<<<<<<< HEAD
-        <h2 className="sr-only">Pagination Navigation</h2>
-=======
         <h2 className="sr-only">Page Navigations</h2>
->>>>>>> testing
 
         <span className="pagination__count">{this.props.current} / {this.props.total}</span>
         {this.props.current !== 1 ? <button type="button" className="pagination__prev">이전 페이지</button> : ''}
