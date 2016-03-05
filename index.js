58a3dbc28481d85dcda79d4fa048391ef5ffa80f
ieunsu <cbcruk@gmail.com>
2016-03-05 12:20:17 +0900

    expree jade 추가

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.set('views', __dirname + '/build');
+app.set('view engine', 'jade');
+
+app.get('/', function(request, response) {
+  response.render('index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
